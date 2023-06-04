0352f447e1af234efad71126dcef915742382310
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-04 23:05:24 +0300

    refactor: move widgets to different classes

--- lib/src/presentation/pages/profile_page.dart
+++ lib/src/presentation/pages/profile_page.dart
@@ -12,11 +12,64 @@ class ProfilePage extends StatelessWidget {
       body: Column(
         children: [
           PanelHeader(),
-          Expanded(
-            child: ListView(),
-          ),
+          Divider(),
+          _Content(),
         ],
       ),
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(children: [
+        Row(
+          children: [
+            CircleAvatar(backgroundImage: AssetImage('assetName')),
+            Column(
+              children: [
+                Text('Алексей Мазелюк'),
+                Text('Санкт-Петербург'),
+              ],
+            ),
+          ],
+        ),
+        Divider(),
+        Row(
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Text('Учавствовать в подборе'),
+                    Spacer(),
+                  ],
+                ),
+                Text(
+                    'Ваш аккаунт будет отображаться в системе подбора игроков'),
+                Switch(value: false, onChanged: null),
+              ],
+            )
+          ],
+        ),
+        Divider(),
+        Row(
+          children: [
+            Icon(Icons.help_outline_outlined),
+            Text('Помощь'),
+          ],
+        ),
+        Row(
+          children: [
+            Icon(Icons.info_outline),
+            Text('О приложении'),
+          ],
+        ),
+      ]),
     );
   }
 }
