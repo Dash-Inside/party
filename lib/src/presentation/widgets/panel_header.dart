02945c6e62231991037fe6ce1d0cb87859bd6716
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-09 22:08:46 +0300

    refactor: remove unnecessary imports

--- lib/src/presentation/widgets/panel_header.dart
+++ lib/src/presentation/widgets/panel_header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:party/src/presentation/widgets/double_button.dart';
 
 class PanelHeader extends StatelessWidget {
