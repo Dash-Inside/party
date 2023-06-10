8de8a149602904a0370cfc956a6ddc5c05e1a9b0
danwirele <115182119+danwirele@users.noreply.github.com>
2023-06-10 13:16:24 +0300

    feat: add event

--- lib/src/presentation/pages/app/bloc/app_bloc.dart
+++ lib/src/presentation/pages/app/bloc/app_bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:party/core/errors/failure.dart';
@@ -11,7 +13,19 @@ part 'app_bloc.freezed.dart';
 class AppBloc extends Bloc<AppEvent, AppState> {
   AppBloc() : super(AppState.loading()) {
     on<LoadAppEvent>(_onLoadAppEvent);
+    on<PageChangeAppEvent>(_onPageChangeAppEvent);
   }
 
-  FutureOr<void> _onLoadAppEvent(LoadAppEvent event, Emitter<AppState> emit) {}
+  FutureOr<void> _onLoadAppEvent(
+    LoadAppEvent event,
+    Emitter<AppState> emit,
+  ) {}
+
+  FutureOr<void> _onPageChangeAppEvent(
+    PageChangeAppEvent event,
+    Emitter<AppState> emit,
+  ) {
+    debugPrint(event.page.toString());
+    emit(AppState.page(page: event.page));
+  }
 }
--- lib/src/presentation/pages/app/bloc/app_event.dart
+++ lib/src/presentation/pages/app/bloc/app_event.dart
@@ -3,4 +3,7 @@ part of 'app_bloc.dart';
 @freezed
 class AppEvent with _$AppEvent {
   const factory AppEvent.load() = LoadAppEvent;
+  const factory AppEvent.onPageChange({
+    required int page,
+  }) = PageChangeAppEvent;
 }
