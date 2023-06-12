7aa34a15dc5d309c6adc20eb6be8a468da8fa337
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-12 16:32:03 +0300

    chore: edit default l10n set values

--- lib/src/presentation/core/l10n/app_l10n.dart
+++ lib/src/presentation/core/l10n/app_l10n.dart
@@ -6,8 +6,8 @@ const AppL10nSettings = const L10nSettings(locales: [
 
 @AppL10nSettings
 class AppL10n {
-  static const appTitle = L10nSet('app_title', 'appTitle');
-  static const searchTabTitle = L10nSet('search_tab_title', 'searchTabTitle');
-  static const teammatesTabTitle = L10nSet('teammates_tab_title', 'teammatesTabTitle');
-  static const etcTabTitle = L10nSet('etc_tab_title', 'etcTabTitle');
+  static const appTitle = L10nSet('app_title', '<AppTitle>');
+  static const searchTabTitle = L10nSet('search_tab_title', '<SearchTabTitle>');
+  static const teammatesTabTitle = L10nSet('teammates_tab_title', '<TeammatesTabTitle>');
+  static const etcTabTitle = L10nSet('etc_tab_title', '<EtcTabTitle>');
 }
